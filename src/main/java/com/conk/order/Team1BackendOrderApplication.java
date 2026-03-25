d0efd30216d3db8bcaa792ac87f210d79013b52c
wjdqudwls <wjdqudwls100@gmail.com>
2026-03-25 17:22:29 +0900

    project setting

---
+++ src/main/java/com/conk/order/Team1BackendOrderApplication.java
@@ -0,0 +1,13 @@
+package com.conk.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team1BackendOrderApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(Team1BackendOrderApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/conk/order/Team1BackendOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.conk.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Team1BackendOrderApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
