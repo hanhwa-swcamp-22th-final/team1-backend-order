b3b8d09e13a38e1e647b7ebd362f9ad4a984a860
ikth-kim <ikth.kim@gmail.com>
2026-04-16 02:17:00 +0900

    order Feign timeout 설정 표준화

--- src/main/java/com/conk/order/common/config/FeignConfig.java
+++ src/main/java/com/conk/order/common/config/FeignConfig.java
@@ -1,6 +1,10 @@
 package com.conk.order.common.config;
 
+import feign.Logger;
+import feign.Request;
 import feign.RequestInterceptor;
+import feign.Retryer;
+import java.util.concurrent.TimeUnit;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -11,6 +15,21 @@ import org.springframework.context.annotation.Configuration;
 public class FeignConfig {
 
     private static final String HEADER_INTERNAL_CALL = "X-Internal-Call";
+
+    @Bean
+    public Logger.Level feignLoggerLevel() {
+        return Logger.Level.BASIC;
+    }
+
+    @Bean
+    public Request.Options feignRequestOptions() {
+        return new Request.Options(5, TimeUnit.SECONDS, 5, TimeUnit.SECONDS, true);
+    }
+
+    @Bean
+    public Retryer feignRetryer() {
+        return Retryer.NEVER_RETRY;
+    }
 
     @Bean
     public RequestInterceptor internalCallRequestInterceptor() {
